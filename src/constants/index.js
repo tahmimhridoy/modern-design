761f94a2b6a04d52c700aa7b429e442d9c4337ac
tahmimhridoy <thamimhasanhridoyea@gmail.com>
2022-09-25 10:26:13 +0600

    navbar and hero section

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,3 @@
-
 import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
 
 export const navLinks = [
