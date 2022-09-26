be3c2d9ad88eab53fbd1b8d40615a4150786c126
tahmimhridoy <thamimhasanhridoyea@gmail.com>
2022-09-26 13:14:32 +0600

    everything is done

--- src/components/Biling.js
+++ src/components/Biling.js
@@ -3,12 +3,12 @@ import {apple, bill, google} from '../assets';
 import styles, {layout} from '../style';
 
 const Biling = () =>(
-    <section id='product' className={layout.sectionReverse}>
-        <div className={layout.sectionImgReverse}>
-            <img src={bill} alt="billing" className='w-[100%] h-[100%] z-[5] relative'/>
+    <section id='product' className={`${layout.sectionReverse}`}>
+        <div className={`${layout.sectionImgReverse} relative`}>
+            <img src={bill} alt="billing" className='w-[100%] h-[100%] z-[5]'/>
 
             <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient"/>
-            <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"/>
+            <div className="absolute z-[0] -left-20 bottom-0 w-[50%] h-[50%] rounded-full blue__gradient "/>
         </div>
 
         <div className={layout.sectionInfo}>
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -5,8 +5,8 @@ import FeedbackCard from './FeedbackCard';
 
 const Testimonial = () => {
     return (
-        <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
-            <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
+        <section id='clients' className={`${styles.paddingY} flex justify-between items-center flex-col relative`}>
+            <div className="absolute z-[0] w-[50%] h-[50%] -right-[40%] rounded-full blue__gradient bottom-40" />
 
             <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
                 <h1 className={styles.heading2}>What people are <br className='sm:block hidden'/> saying about us</h1>
